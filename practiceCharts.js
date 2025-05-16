d809bfe04576cb81f53a41b652c66fb518945eff
Sara5722 <143350752+Sara5722@users.noreply.github.com>
2025-05-16 12:07:19 -0500

    added colors and scatterplot with avg line

--- practiceCharts.js
+++ practiceCharts.js
@@ -7,9 +7,11 @@ new Chart(barCanvas, {
   data:{
     labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
     datasets:[{
-        label: 'Steps',
+        label: 'Steps by Week',
         data: [12000, 5000, 8000, 11000, 5000, 5000, 10000],
         borderWidth: 1,
+        backgroundColor: 
+            'rgba(255, 99, 132, 0.5)' 
     }]
   },
     options: {
@@ -29,7 +31,7 @@ new Chart(lineCanvas, {
   data:{
     labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
     datasets:[{
-        label: 'Steps',
+        label: 'Steps by Week',
         data: [12000, 5000, 8000, 11000, 5000, 5000, 10000],
         borderWidth: 1,
     }]
@@ -45,24 +47,49 @@ new Chart(lineCanvas, {
 )
 
 
-// const scatterCanvas = document.getElementById('scatter').getContext('2d'); 
+const scatterCanvas = document.getElementById('scatter').getContext('2d'); 
 
-// new Chart(scatterCanvas, {
-//   type: 'scatter',
-//   data:{
-//     labels: [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday],
-//     datasets:[{
-//         label: 'Steps',
-//         data: [12000, 5000, 8000, 11000, 5000, 5000, 10000],
-//         borderWidth: 1,
-//     }]
-//   },
-//     options: {
-//         scales: {
-//         y: {
-//             beginAtZero: true
-//         }
-//         }
-//     }   
-// }
-// )
+new Chart(scatterCanvas, {
+  data:{
+  datasets:[{
+    type: 'scatter',
+    label: 'Steps by Week',
+    data: [
+      { x: 0, y: 12000 },
+      { x: 1, y: 5000 },
+      { x: 2, y: 8000 },
+      { x: 3, y: 11000 },
+      { x: 4, y: 5000 },
+      { x: 5, y: 5000 },
+      { x: 6, y: 10000 }
+    ],
+    borderWidth: 1,
+    backgroundColor: 'rgba(60, 145, 21, 0.7)',
+  }, {
+    type: 'line',
+    label: 'Average Steps',
+    data: [
+      { x: 0, y: 8000 },
+      { x: 1, y: 8000 },
+      { x: 2, y: 8000 },
+      { x: 3, y: 8000 },
+      { x: 4, y: 8000 },
+      { x: 5, y: 8000 },
+      { x: 6, y: 8000 }
+    ],
+    borderWidth: 2,
+    backgroundColor: 'rgba(254, 34, 14, 0.5)',
+    pointRadius: 3,
+  
+  }]
+},
+    options: {
+        scales: {
+        y: {
+            beginAtZero: true
+        }
+        }
+    }   
+}
+)
+
